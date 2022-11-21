a7d8861c760d9cf88d31573956ee01c53f5afa51
Mark Eibes <mark.eibes@gmail.com>
2022-11-21 09:48:48 +0100

    Reexports for typeahead

--- src/Yoga/Block/Molecule/Typeahead.purs
+++ src/Yoga/Block/Molecule/Typeahead.purs
@@ -1,1 +1,5 @@
-module Plumage.Molecule.Typeahead where
+module Yoga.Block.Molecule.Typeahead
+  ( module Yoga.Block.Molecule.Typeahead.View
+  ) where
+
+import Yoga.Block.Molecule.Typeahead.View (Args, InputProps, Props, ScrollSeekConfiguration, inputProps, mkDefaultArgs, mkTypeahead, mkTypeaheadView)
