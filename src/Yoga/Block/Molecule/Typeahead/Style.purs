7b6a95bec96e56b99199db8705e8d2c0e76f0f4e
Mark Eibes <mark.eibes@gmail.com>
2022-12-06 09:34:57 +0100

    Reuse popOverStyle

--- src/Yoga/Block/Molecule/Typeahead/Style.purs
+++ src/Yoga/Block/Molecule/Typeahead/Style.purs
@@ -8,28 +8,7 @@ type Props f r = (| r)
 
 resultsContainer ∷ Style
 resultsContainer =
-  pXY 0
-    <> mT 4
-    <> mB 4
-    <> maxHeight' (50.0 # vh)
-    <> minHeight' (25.0 # vh)
-    <> height' (230 # px)
-    <> justifyEnd
-    <> itemsStart
-    <> gap 3
-    <> roundedDefault
-    <> shadowXxl
-    <> overflowHidden
-    <> blurredBackground'
-      { blurRadius: 4
-      , blurredCol: colourWithDarkLightAlpha.backgroundLayer5
-          { darkAlpha: 0.5, lightAlpha: 0.5 }
-      , fallbackCol: colour.inputBackground
-      }
-    <> border 1
-    <> borderSolid
-    <> borderCol' col.inputBorder
-    <> PopOverStyle.popOverStyle
+  PopOverStyle.popOverStyle
     <> css { ".TypeaheadList": nested (pXY 0 <> mXY 0) }
 
 resultContainer ∷ Style
